2db37c56966289bd00142ce9599cde0c5ed34b9c
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-03 11:17:34 -0800

    add: docs folder, l01, and hw0 pdf

---
+++ l01/hello.cpp
@@ -0,0 +1,23 @@
+// hello.cpp
+// Solomon Himelbloom
+// 31 August 2020
+// Hello world lab example for CS 201.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+    cout << "Hello, world." << endl;
+    cout << "Hello, world." << endl;
+    cout << "Hello, world." << endl;
+    cout << "Hello, world." << endl;
+    cout << "Hello, world." << endl;
+
+    cout << "Press ENTER to quit ";
+    while (cin.get() != '\n') ;
+
+    return 0;
+}

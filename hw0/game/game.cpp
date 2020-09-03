c6f54ff6ead2c3757f6950661d5eb0338cdd9f8b
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-02 20:54:16 -0800

    Add Robert Frost poem and Tetris game description

--- hw0/game/game.cpp
+++ hw0/game/game.cpp
@@ -9,5 +9,10 @@ using std::endl;
 
 int main()
 {
+    // Tetris (video game, est. 1984)
+    // Further Reading: https://www.britannica.com/topic/Tetris & https://en.wikipedia.org/wiki/Tetris
+    cout << "Tetris is a single-player game, which is available on almost every operating system." << endl;
+    cout << "Players need to arrange and rotate unique shaped blocks that drop at different speeds." << endl;
+    cout << "The game ends when blocks reach the top of the screen." << endl;
     return 0;
 }
--- hw0/poem/poem.cpp
+++ hw0/poem/poem.cpp
@@ -9,5 +9,11 @@ using std::endl;
 
 int main()
 {
+    // The Road Not Taken by Robert Frost
+    cout << "I shall be telling this with a sigh" << endl;
+    cout << "Somewhere ages and ages hence:" << endl;
+    cout << "Two roads diverged in a wood, and I—" << endl;
+    cout << "I took the one less traveled by," << endl;
+    cout << "And that has made all the difference." << endl;
     return 0;
 }

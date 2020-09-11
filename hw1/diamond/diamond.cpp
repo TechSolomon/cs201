62bc400bfe2cae6fb1c6d1178b21257977a4db02
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-11 03:06:37 -0800

    update: diamond if/else logic and for loops

--- hw1/diamond/diamond.cpp
+++ hw1/diamond/diamond.cpp
@@ -9,5 +9,35 @@ using std::endl;
 using std::cin;
 
 int main() {
-    cout << "Hello, world." << endl;
+    int diamond_column, diamond_row, user_input;
+
+    cout << "Type in a positive integer: ";
+    cin >> user_input;
+
+    if (user_input <= 0) {
+        cout << "You typed in zero, a negative number, or a letter." << endl;
+        cout << "Please try again with a positive integer." << endl;
+    }
+
+    else {
+        for (diamond_row = 0; diamond_row <= user_input; diamond_row++) {
+            for (diamond_column = 1; diamond_column <= user_input - diamond_row; diamond_column++) {
+                cout << " ";
+            }
+            for (diamond_column = 1; diamond_column <= 2 * diamond_row - 1; diamond_column++) {
+                cout << "#";
+            }
+            cout << endl;
+        }
+
+        for (diamond_row = user_input - 1; diamond_row >= 1; diamond_row--) {
+            for (diamond_column = 1; diamond_column <= user_input - diamond_row; diamond_column++) {
+                cout << " ";
+            }
+            for (diamond_column = 1; diamond_column <= 2 * diamond_row - 1; diamond_column++) {
+                cout << "#";
+            }
+            cout << endl;
+        } 
+    }
 }

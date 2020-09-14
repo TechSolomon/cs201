b847c10375b8824bfa9b3ea14f76fd7067f3e622
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-14 00:18:50 -0800

    refactor: l05 user input and final build hw1

--- labs/l05/switch.cpp
+++ labs/l05/switch.cpp
@@ -9,32 +9,45 @@ using std::endl;
 using std::cin;
 
 int main() {
-    cout << "Hello, world." << endl;
 
-    int n;
+    int n, first_number, second_number;
+    int addition, subtraction, multiplication, division = 0;
 
-    switch (n % 10) 
-    {
-    case 2:
-        cout << "two";
-        break;
+    cout << "Enter the 1st number: ";
+    cin >> first_number;
 
+    cout << "Enter the 2nd number: ";
+    cin >> second_number;
+
+    switch (n % 10) {
     case 1:
-        cout << "one";
+        addition = (first_number + second_number);
+        cout << first_number << " + " << second_number << " = " << addition << endl;
+        break;
+    
+    case 2:
+        subtraction = (first_number - second_number);
+        cout << first_number << " - " << second_number << " = " << subtraction << endl;
         break;
 
     case 3:
-        cout << "BLAH BLAH";
+        multiplication = (first_number * second_number);
+        cout << first_number << " * " << second_number << " = " << multiplication << endl;
+        break;
+
+    case 4:
+        division = (first_number / second_number);
+        cout << first_number << " / " << second_number << " = " << division << endl;
         break;
     
     default:
-        cout << "Something else.";
+        cout << "Lucky you! This is the default switch to print something else." << endl;
         break;
     }
 
-    cout << " " << endl;
-    cout << "Press ENTER to quit ";
-    while (cin.get() != '\n') ;
+    // cout << " " << endl;
+    // cout << "Press ENTER to quit ";
+    // while (cin.get() != '\n');
 
     return 0;
 }

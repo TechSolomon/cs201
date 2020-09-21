94ca037470408873817f07e1d116dac471da67df
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-20 21:50:04 -0800

    add: hw2 rice blank template files

---
+++ hw2/rice/rice.cpp
@@ -0,0 +1,15 @@
+// rice.cpp
+// Solomon Himelbloom
+// 20 September 2020
+// Rice & Chessboard (exponential growth) example for CS 201.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+using namespace std;
+
+int main() {
+    cout << "The Rice & Chessboard Story: Exponential Growth" << endl;
+}

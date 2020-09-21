b2c91987692a4c5cdec48251d2de0e90ce5dd980
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-20 21:14:00 -0800

    add: hw2 scores blank template files

---
+++ hw2/scores/scores.cpp
@@ -0,0 +1,18 @@
+// scores.cpp
+// Solomon Himelbloom
+// 20 September 2020
+// Name-and-value pairs (scores) example for CS 201.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+using namespace std;
+
+int main() {
+    cout << "Name and Value Pairs:" << endl;
+    cout << "Joe | 17" << endl;
+    cout << "Solomon | 19" << endl;
+    cout << "Barbara | 22" << endl;
+}

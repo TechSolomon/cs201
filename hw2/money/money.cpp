75fb9b22c46ae61f771f43ccac9fef43056ea95f
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-20 20:30:27 -0800

    add: bankAccount function and total logic

--- hw2/money/money.cpp
+++ hw2/money/money.cpp
@@ -8,16 +8,47 @@ using std::cout;
 using std::endl;
 using std::cin;
 
-// SAMPLE OUTPUT:
-// You have 23 pennies.
-// You have 17 nickels.
-// You have 14 dimes.
-// You have 7 quarters.
-// You have 3 half dollars.
-// The value of all your coins is 537 cents.
+using namespace std;
+
+void bankAccount(string coins, float money) {
+    cout << "How many " << coins << " do you have? ";
+}
 
 int main() {
-    cout << "The value of all your coins is x cents." << endl;
+    float pennies, nickels, dimes, quarters, half_dollars, cents = 0;
+    float bank_account = 0;
+
+    bankAccount("pennies", pennies);
+    cin >> pennies;
+
+    bankAccount("nickels", nickels);
+    cin >> nickels;
+
+    bankAccount("dimes", dimes);
+    cin >> dimes;
+
+    bankAccount("quarters", quarters);
+    cin >> quarters;
+
+    bankAccount("halfdollars", half_dollars);
+    cin >> half_dollars;
+
+    cout << " " << endl;
+    cout << "You have " << pennies << " pennies." << endl;
+    cout << "You have " << nickels << " nickels." << endl;
+    cout << "You have " << dimes << " dimes." << endl;
+    cout << "You have " << quarters << " quarters." << endl;
+    cout << "You have " << half_dollars << " half dollars." << endl;
+
+    pennies = 0.01*pennies;
+    nickels = 0.05*nickels;
+    dimes = 0.10*dimes;
+    quarters = 0.25*quarters;
+    half_dollars = 0.50*half_dollars;
+
+    bank_account = (pennies + nickels + dimes + quarters + half_dollars);
+    cout << " " << endl;
+    cout << "The value of all your coins is: $" << bank_account << endl;
 
     return 0;
 }

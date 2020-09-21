630073ca1f5bf83963dec4effa2f8ad8dad0b273
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-20 19:45:09 -0800

    add: hw2 money sample output

---
+++ hw2/money/money.cpp
@@ -0,0 +1,23 @@
+// money.cpp
+// Solomon Himelbloom
+// 20 September 2020
+// Money example for CS 201.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+// SAMPLE OUTPUT:
+// You have 23 pennies.
+// You have 17 nickels.
+// You have 14 dimes.
+// You have 7 quarters.
+// You have 3 half dollars.
+// The value of all your coins is 537 cents.
+
+int main() {
+    cout << "The value of all your coins is x cents." << endl;
+
+    return 0;
+}

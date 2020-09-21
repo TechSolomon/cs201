ba788797dc8520c47118fce6f88cfbec649216b8
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-20 20:53:38 -0800

    refactor: bank_account and add: coinReceipt
    Changes also add one-dollar coins and attempt to fix DRY issues.

--- hw2/money/money.cpp
+++ hw2/money/money.cpp
@@ -10,13 +10,17 @@ using std::cin;
 
 using namespace std;
 
-void bankAccount(string coins, float money) {
-    cout << "How many " << coins << " do you have? ";
+void bankAccount(string coinType, float numberOfCoins) {
+    cout << "How many " << coinType << " do you have? ";
+}
+
+void coinReceipt(float numberOfCoins, string coinType) {
+    cout << "You have " << numberOfCoins << " " << coinType << "." << endl;
 }
 
 int main() {
-    float pennies, nickels, dimes, quarters, half_dollars, cents = 0;
-    float bank_account = 0;
+    float pennies, nickels, dimes, quarters, half_dollars, one_dollars = 0;
+    float total_coins = 0;
 
     bankAccount("pennies", pennies);
     cin >> pennies;
@@ -33,22 +37,29 @@ int main() {
     bankAccount("halfdollars", half_dollars);
     cin >> half_dollars;
 
-    cout << " " << endl;
-    cout << "You have " << pennies << " pennies." << endl;
-    cout << "You have " << nickels << " nickels." << endl;
-    cout << "You have " << dimes << " dimes." << endl;
-    cout << "You have " << quarters << " quarters." << endl;
-    cout << "You have " << half_dollars << " half dollars." << endl;
+    bankAccount("one-dollar coins", one_dollars);
+    cin >> one_dollars;
 
+    // Print out the total number and type of each coin.
+    cout << " " << endl;
+    coinReceipt(pennies, "pennies");
+    coinReceipt(nickels, "nickels");
+    coinReceipt(dimes, "dimes");
+    coinReceipt(quarters, "quarters");
+    coinReceipt(half_dollars, "half dollars");
+    coinReceipt(one_dollars, "one-dollar coins");
+
+    // Convert number of coins to dollars.
     pennies = 0.01*pennies;
     nickels = 0.05*nickels;
     dimes = 0.10*dimes;
     quarters = 0.25*quarters;
     half_dollars = 0.50*half_dollars;
+    one_dollars = 1.00*one_dollars; 
 
-    bank_account = (pennies + nickels + dimes + quarters + half_dollars);
     cout << " " << endl;
-    cout << "The value of all your coins is: $" << bank_account << endl;
+    total_coins = (pennies + nickels + dimes + quarters + half_dollars + one_dollars);
+    cout << "The value of all your coins is: $" << total_coins << endl;
 
     return 0;
 }

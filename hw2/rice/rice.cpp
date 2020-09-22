2cd54e6d5f6297cbf3cc6a4537270981b271a1cb
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-21 22:33:21 -0800

    add: double current_square, previous_square, and user input

--- hw2/rice/rice.cpp
+++ hw2/rice/rice.cpp
@@ -1,28 +1,53 @@
 // rice.cpp
 // Solomon Himelbloom
 // 20 September 2020
-// Rice & Chessboard (exponential growth) example for CS 201.
+// Rice and chessboard (exponential growth) example for CS 201.
 
 #include <iostream>
+#include <math.h>
 using std::cout;
 using std::endl;
 using std::cin;
 
 using namespace std;
 
+double current_square (int square) {
+    return pow(2, square - 1);
+}
+
+double previous_square (int square) {
+    double total = 0;
+    for (int i = 1; i <= square; i++) {
+        total = current_square(i);
+    }
+
+    return total;
+}
+
 int main() {
     int i = 1;
-    int current_square, previous_square = 0;
+    int requested_rice, current_square, previous_square, total_rice = 0;
+
+    cout << "MESSAGE TO INVENTOR: What is your desired amount of rice? ";
+    cin >> requested_rice;
 
     current_square = 0;
     previous_square = 0;
+    total_rice = 0;
 
-    for (i = 1; i < 4; i++) {
+    cout << " " << endl;
+    cout << "MESSAGE TO EMPEROR: Grains of rice requested: " << requested_rice << endl; 
+    cout << " " << endl;
+
+    for (i = 1; i < requested_rice; i++) {
 
         current_square += 1;
+        previous_square = current_square * current_square;
+        total_rice = current_square + previous_square;
 
-        cout << "Current Square: " << current_square << endl;
+        cout << "Current Square #" << current_square << endl;
         cout << "Previous Square: " << previous_square << endl;
+        cout << "Total Rice: " << total_rice << endl;
         cout << " " << endl;
     }
 

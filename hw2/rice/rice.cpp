a13cab50b95944ff8a4704945a024664a50d34c2
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-21 23:47:06 -0800

    edit: user input/output

--- hw2/rice/rice.cpp
+++ hw2/rice/rice.cpp
@@ -28,28 +28,28 @@ int main() {
     int i = 1;
     int requested_rice, current_square, previous_square, total_rice = 0;
 
-    cout << "MESSAGE TO INVENTOR: What is your desired amount of rice? ";
+    cout << "NEW MESSAGE (INVENTOR -> EMPEROR): What is your desired amount of rice? ";
     cin >> requested_rice;
 
     current_square = 0;
     previous_square = 0;
     total_rice = 0;
 
-    cout << " " << endl;
-    cout << "MESSAGE TO EMPEROR: Grains of rice requested: " << requested_rice << endl; 
-    cout << " " << endl;
-
     for (i = 1; i < requested_rice; i++) {
 
         current_square += 1;
         previous_square = current_square * current_square;
-        total_rice = current_square + previous_square;
+        total_rice += current_square + previous_square;
 
         cout << "Current Square #" << current_square << endl;
         cout << "Previous Square: " << previous_square << endl;
         cout << "Total Rice: " << total_rice << endl;
         cout << " " << endl;
     }
+
+    cout << " " << endl;
+    cout << "NEW MESSAGE (EMPEROR -> INVENTOR): Grains of rice requested: " << requested_rice << endl; 
+    cout << " " << endl;
 
     return 0;
 }

c717a4c8adc8f18635c87e97ee091e5d988a763b
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-22 21:57:59 -0800

    add: print, search, encrypt, sort, and countNames function

--- hw2/names/names.cpp
+++ hw2/names/names.cpp
@@ -13,28 +13,49 @@ using std::cin;
 
 using namespace std;
 
-void codeEncapsulation() {
-    void InputNames(std::vector<std::string> & names);
-}
-
-// void nameSearch(bool) { 
-//     bool DoesNameExist(const std::string & nametoFind, const std::vector<std::string> & names);
-
-//     return true;
-// }
-
-void PrintNames(string Names) {
-    bool DoesNameExist(const std::string & nametoFind, const std::vector<std::string> & names);
-}
-
-int main (int argc, char ** argv) {
-    std::vector<std::string> names;
+void inputNames(std::vector<std::string> & names) {
     for (int i = 0; i < 10; i++) {
         std::string name;
         std::cout << "Please enter a name: ";
         std:getline(std::cin, name);
         names.push_back(name);
     }
+}
+
+bool doesNameExist(const std::string & nametoFind, const std::vector<std::string> & names) {
+    return true;
+}
+
+void printNames(std::vector<std::string> & names) {
+    cout << "This prints the names that the user provided." << endl;
+}
+
+void searchNames(std::vector<std::string> & names) { 
+    cout << "Need a query? This allows for easy searchability of the names." << endl;
+}
+
+void encryptNames(std::vector<std::string> & names) {
+    cout << "This encrypts the aformentioned names using a cipher." << endl;
+}
+
+void sortNames(std::vector<std::string> & names) { 
+    cout << "COMING SOON: Use computer science sorting algorithms to make your job easier!" << endl;
+}
+
+void countNames(std::vector<std::string> & names) { 
+    cout << "Learn the number of characters in each string." << endl;
+}
+
+int main (int argc, char ** argv) {
+    std::vector<std::string> names;
+
+    inputNames(names);
+    
+    printNames(names);
+    searchNames(names);
+    encryptNames(names);
+    sortNames(names);
+    countNames(names);
     
     return 0;
 }

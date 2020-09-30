1c35c7b381d7fb8e6c63b6c74bf974a0fc3b7432
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-30 03:13:47 -0800

    add: boilerplate cpp STL headers

---
+++ hw3/stl/std_lib_facilities.h
@@ -0,0 +1,13 @@
+// std_lib-facilities.hpp
+// Solomon Himelbloom
+// 30 September 2020
+// Standard Template Library (STL) header file example for CS 201.
+
+#ifndef FILE_STD_LIB_FACILITIES_TEST_INCLUDED
+#define FILE_FACILITIES_TEST_HPP_INCLUDED
+
+void keep_window_open() {
+    // insert code here
+}
+
+#endif
--- hw3/stl/std_lib_facilities_test.cpp
+++ hw3/stl/std_lib_facilities_test.cpp
@@ -3,10 +3,30 @@
 // 23 September 2020
 // Standard Template Library (STL) example for CS 201.
 
+// #include "std_lib_facilities.h"
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+// #include <map>
+// #include <cstdio>
+// #include <cstdlib>
+// #include <cmath>
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
+using std::vector;
+
+inline void keep_window_open() {
+    std::string tmp;
+    cin.clear();
+
+    cout << "Press ENTER to exit the program." << endl;
+    std::getline(cin, tmp);
+}
 
 int main() {
     cout << "Standard Template Library (STL)." << endl;

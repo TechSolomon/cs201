6b63970cd7c314e91433fbe8b85c7f0d5e678377
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-30 21:03:20 -0800

    add: discriminant, testing; include: math

--- hw3/quadratic/quadratic.cpp
+++ hw3/quadratic/quadratic.cpp
@@ -4,19 +4,28 @@
 // Quadratic formula for CS 201.
 
 #include <iostream>
+#include <math.h>
 using std::cout;
 using std::endl;
 using std::cin;
 
 void quadraticFormula(int a, int b, int c) {
-    cout << "This is what a quadratic formula looks like..." << endl;
-    cout << "x = -b +/- sqrt(b^2-4ac)/(2a)" << endl;
-    cout << " " << endl;
+    float discriminant;
+    discriminant = b*b - 4*a*c;
+
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "c = " << c << endl;
+    cout << "discriminant = " << discriminant << endl;
+
+    cout << " " << endl;
 }
 
+// QUADRATIC FORMULA: x = -b +/- sqrt(b^2-4ac)/(2a)
 int main() {
+    cout << "" << endl;
+
     quadraticFormula(1, 2, 3);
+    quadraticFormula(4, 5, 6);
+    quadraticFormula(7, 8, 9);
 }

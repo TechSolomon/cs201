3f040d9450bcc68e9684107d8d13dc0b595a98a5
Solomon Himelbloom <2001solomon@gmail.com>
2020-09-30 21:33:10 -0800

    add: support for non-real roots

--- hw3/quadratic/quadratic.cpp
+++ hw3/quadratic/quadratic.cpp
@@ -9,27 +9,45 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+// QUADRATIC FORMULA: x = -b +/- sqrt(b^2-4ac)/(2a)
 void quadraticFormula(int a, int b, int c) {
     float discriminant, x1, x2;
     discriminant = b*b - 4*a*c;
-    x1 = (-b + sqrt(discriminant)) / (2*a);
-    x2 = (-b - sqrt(discriminant)) / (2*a);
 
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "c = " << c << endl;
-    cout << "discriminant = " << discriminant << endl;
-    cout << "x1 = " << x1 << endl;
-    cout << "x2 = " << x2 << endl;
 
-    cout << " " << endl;
+    if (discriminant == 0) {
+        cout << "Error: The resulting discriminant (b^2-4ac) is zero." << endl;
+        cout << "Please try again." << endl;
+        cout << " " << endl;
+    }
+
+    else if (discriminant < 0) {
+        cout << "Fail: The resulting discriminant (b^2-4ac) is negative." << endl;
+        cout << "The quadratic equation has no real roots." << endl;
+        cout << " " << endl;
+    }
+    
+    else {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+        cout << " " << endl;
+    }
+    
 }
 
-// QUADRATIC FORMULA: x = -b +/- sqrt(b^2-4ac)/(2a)
 int main() {
-    cout << "" << endl;
-
+    // Pass: Discriminant is positive.
     quadraticFormula(1, 4, 3);
-    quadraticFormula(1, -9, 8);
-    quadraticFormula(-1, 8, -15);
+
+    // Fail: Discriminant is negative.
+    quadraticFormula(-5, 1, -8);
+
+    // Check: Discriminant is zero.
+    quadraticFormula(0, 0, 1);
 }

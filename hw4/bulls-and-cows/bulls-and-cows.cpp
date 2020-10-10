f01143062935a43e88241f6b788d9a3811d1cdcf
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-09 18:51:24 -0800

    add: randomNumber and if/else to bulls-and-cows.cpp

--- hw4/bulls-and-cows/bulls-and-cows.cpp
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -14,19 +14,31 @@ using std::endl;
 using std::string;
 using std::vector;
 
+void randomNumber() {
+    int random_number = 1357;
+    cout << "Random integer: " << random_number << endl;
+}
+
 void guessingGame() {
     cout << "1 bull and 1 cow." << endl;
 }
 
 int main(int argc, char *argv[]) {
     int user_input = 0;
+    int random_number = 1357;
 
     std::vector<std::string> numbers;
 
     cout << "Enter a four digit number (no repeats & negative to quit): ";
     cin >> user_input;
 
-    guessingGame();
+    if (user_input == random_number) {
+        randomNumber();
+    }
+    
+    else {
+        guessingGame();
+    }
 
     return 0;
 }

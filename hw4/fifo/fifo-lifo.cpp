b9224ee87b18fd4369912fa61ff5e4675cdf8a3c
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-09 18:23:25 -0800

    add: Fifo, Lifo, Push, Pop function templates
    Includes templated comments for IsContainerEmpty, PrintContainer, TestFifo, and TestLifo.

--- hw4/fifo/fifo-lifo.cpp
+++ hw4/fifo/fifo-lifo.cpp
@@ -14,6 +14,25 @@ using std::endl;
 using std::string;
 using std::vector;
 
+// First-in First-Out
+void FifoPush(vector<string> &container, const string &item);
+void FifoPop(vector<string> &container, string &item);
+
+// Last-In First-Out
+void LifoPush(vector<string> &container, const string &item);
+void LifoPop(vector <string> &container, string &item);
+
+// Shared functionality
+bool IsContainerEmpty(const vector<string> &container);
+void PrintContainer(const vector<string> &container);
+
+// Implement these two tests to verify your functions work
+// with at least the sequence:
+//      push "A", push "B", push "C", push "D"
+//      push    , pop     , pop     , pop
+bool TestFifo();
+bool TestLifo();
+
 int main() {
     cout << "Hello, FIFO." << endl;
 }

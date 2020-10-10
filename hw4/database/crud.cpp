1bdce81963af81f1faa667c8e686cf804028d1d6
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-09 17:42:33 -0800

    add: crud.cpp and database.cpp template

---
+++ hw4/database/crud.cpp
@@ -0,0 +1,15 @@
+// crud.cpp
+// Solomon Himelbloom
+// 9 October 2020
+// Database [crud] example for CS 201.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
---
+++ hw4/database/database.cpp
@@ -0,0 +1,19 @@
+// database.cpp
+// Solomon Himelbloom
+// 7 October 2020
+// Database example for CS 201.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, database." << endl;
+}

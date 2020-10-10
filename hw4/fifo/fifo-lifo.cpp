6d015e41ece39c606b308ca35cd1df013c7a5fc0
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-09 18:17:57 -0800

    add: fifo-lifo.cpp template file

---
+++ hw4/fifo/fifo-lifo.cpp
@@ -0,0 +1,19 @@
+// fifo-lifo.cpp
+// Solomon Himelbloom
+// 9 October 2020
+// Tokenizer example for CS 201.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, FIFO." << endl;
+}

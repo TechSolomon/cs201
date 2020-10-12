c5305d6f1890a68f5036bbf5f95ac04f6a0531a6
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-11 22:04:00 -0800

    add: numberVector and guessVector

--- hw4/bulls-and-cows/bulls-and-cows.cpp
+++ hw4/bulls-and-cows/bulls-and-cows.cpp
@@ -14,20 +14,31 @@ using std::endl;
 using std::string;
 using std::vector;
 
-void randomNumber() {
+void randomNumber() { 
     int random_number = 1357;
     cout << "Random integer: " << random_number << endl;
 }
 
 void guessingGame() {
+    int bulls = 0;
+    int cows = 0;
+
+    vector<int> numberVector(4);
+    vector<char> guessVector(0);
+    cout << "numberVector size: " << numberVector.size() << endl;
+    cout << "guessVector size: " << guessVector.size() << endl;
+
+    for (auto i = 0; i < numberVector.size(); ++i) {
+        cout << numberVector[i] << " ";
+    }
+    
+    cout << endl;
     cout << "1 bull and 1 cow." << endl;
 }
 
 int main(int argc, char *argv[]) {
     int user_input = 0;
     int random_number = 1357;
-
-    std::vector<std::string> numbers;
 
     cout << "Enter a four digit number (no repeats & negative to quit): ";
     cin >> user_input;

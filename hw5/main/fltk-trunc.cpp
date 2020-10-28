64484052d189d6c6e9eae5e801b722fe4e6a571e
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-28 01:56:53 -0800

    add: hw5 (initial files)

---
+++ hw5/main/fltk-trunc.cpp
@@ -0,0 +1,13 @@
+// fltk-trunc.cpp
+// Solomon Himelbloom
+// 28 October 2020
+// FLTK library example for CS 201.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main() {
+    cout << "Hello, FLTK." << endl;
+}

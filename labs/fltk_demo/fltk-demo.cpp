c00aa4d45fd515a425c759216d926c9d2e94f691
Solomon Himelbloom <2001solomon@gmail.com>
2020-10-28 03:50:00 -0800

    refactor: fltk-demo.cpp

--- labs/fltk_demo/fltk-demo.cpp
+++ labs/fltk_demo/fltk-demo.cpp
@@ -1,10 +1,15 @@
+// fltk-demo.cpp
+// Solomon Himelbloom
+// 28 October 2020
+// FLTK library example for CS 201.
+
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
 
 int main(int argc, char **argv) {
     Fl_Window *window = new Fl_Window(340,180);
-    Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
+    Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, Solomon!");
     box->box(FL_UP_BOX);
     box->labelfont(FL_BOLD+FL_ITALIC);
     box->labelsize(36);

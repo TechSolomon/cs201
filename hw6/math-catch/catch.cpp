e7d548512f3da448349a0c9e8bcc74604d9d964c
Solomon Himelbloom <2001solomon@gmail.com>
2020-11-16 19:05:21 -0900

    add: template catch.cpp and catch_amalgamated
    https://github.com/catchorg/Catch2/blob/devel/docs/tutorial.md#getting-catch2

---
+++ hw6/math-catch/catch.cpp
@@ -0,0 +1,26 @@
+// catch.cpp
+// Solomon Himelbloom
+// 11 November 2020
+// Math catch for CS 201.
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch_amalgamated.hpp"
+
+unsigned int Factorial( unsigned int number ) {
+    return number > 1 ? Factorial(number-1)*number : 1;
+}
+
+// TEST_CASE( "Factorials are computed", "[factorial]" ) {
+//     REQUIRE( Factorial(1) == 1 );
+//     REQUIRE( Factorial(2) == 2 );
+//     REQUIRE( Factorial(3) == 6 );
+//     REQUIRE( Factorial(10) == 3628800 );
+// }
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}

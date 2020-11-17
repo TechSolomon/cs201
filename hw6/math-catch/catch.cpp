3e63abcc8e2da188b003e4d8476b71d962420ab1
Solomon Himelbloom <2001solomon@gmail.com>
2020-11-16 20:14:10 -0900

    add: accumulate and inner_product tests (catch.cpp)

--- hw6/math-catch/catch.cpp
+++ hw6/math-catch/catch.cpp
@@ -3,9 +3,10 @@
 // 11 November 2020
 // Math catch for CS 201.
 
-#include <cmath>
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+// This tells Catch to provide a main() - only do this in one cpp file.
+#define CATCH_CONFIG_MAIN
 #include "catch_amalgamated.hpp"
+#include <cmath>
 
 unsigned int Factorial( unsigned int number ) {
     return number > 1 ? Factorial(number-1)*number : 1;
@@ -32,4 +33,19 @@ TEST_CASE ( "STL Unit Testing: atan2(x)", "[arc-tangant function]" ) {
     REQUIRE(round(atan2(M_PI, 2)) == 1);
     REQUIRE(round(atan2(M_PI, 4)) == 1);
 }
-           
+
+TEST_CASE ( "STL Unit Testing: accumulate(b,e,i)", "[accumulate function]" ) {
+    int fibannaci[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377};
+    REQUIRE(std::accumulate(fibannaci+9, fibannaci+10, 0) == 34);
+    REQUIRE(std::accumulate(fibannaci+10, fibannaci+11, 0) == 55);
+    REQUIRE(std::accumulate(fibannaci+11, fibannaci+12, 0) == 89);
+    REQUIRE(std::accumulate(fibannaci+12, fibannaci+13, 0) == 144);
+}
+
+TEST_CASE ( "STL Unit Testing: inner_product(b,e,b2,i)", "[inner-product function]" ) {
+    int inner_int = 100, even[] = {20, 30, 40}, odd[] = {1, 2, 3};
+    REQUIRE(std::inner_product(even, odd+3, odd, inner_int) == 100);
+    REQUIRE(std::inner_product(even, odd+3, odd, inner_int) == 100);
+    REQUIRE(std::inner_product(even, odd+3, odd, inner_int) == 100);
+    REQUIRE(std::inner_product(even, odd+3, odd, inner_int) == 100);
+}

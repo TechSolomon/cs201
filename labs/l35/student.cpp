d360d9959459cfab1110055ca5927875b1ba48b5
Solomon Himelbloom <2001solomon@gmail.com>
2020-11-30 10:25:21 -0900

    add: draft l34, l35, and l36 (final labs)
    L34 – Classes and Constructors
    L35 – Separate Class Header and Source Files
    L36 – Bits and Flags

---
+++ labs/l35/student.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file     student.cpp
+ * @author   Solomon Himelbloom
+ * @version  November 30, 2020
+ *
+ * Source file for the main students program.
+ */
+
+// using M_PI for pi
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <vector>
+
---
+++ labs/l35/student.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file     student.hpp
+ * @author   Solomon Himelbloom
+ * @version  November 30, 2020
+ *
+ * Header file for the main students program.
+ */
+
+#ifndef STUDENT_HPP_
+#define STUDENT_HPP_
+
+#include <vector>
+
+std::vector<double> degreesToRadians(int degree);
+
+#endif /* STUDENT_HPP_ */

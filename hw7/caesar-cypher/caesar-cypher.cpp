268d4d8ffc2a3087881c3b5e53ef7b74f1bf1425
Solomon Himelbloom <2001solomon@gmail.com>
2020-12-01 21:38:35 -0900

    add: user input and userRequestedInput func

--- hw7/caesar-cypher/caesar-cypher.cpp
+++ hw7/caesar-cypher/caesar-cypher.cpp
@@ -13,6 +13,23 @@ using std::endl;
 using std::string;
 using std::map;
 
+void userRequestedInput() {
+    std::string message_input = "";
+    int integer_input = 0;
+    
+    cout << "\nEnter a message to cypher (blank line to end): ";
+    cin >> message_input;
+
+    cout << "Enter an integer to use as the shift: ";
+    cin >> integer_input;
+
+    cout << "\nResult: TODO";
+    cout << "\nMessage Input = " << message_input;
+    cout << "\nInteger Input = " << integer_input;
+}
+
 int main() {
-    std::cout << "Hello, Caesar Cypher." << endl;
+    cout << "Caesar Cypher" << endl;
+    userRequestedInput();
+    cout << "\n\nProgram Complete" << endl;
 }

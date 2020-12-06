896b4c0297ce60455df653c5f5cb89d49b903e53
Solomon Himelbloom <2001solomon@gmail.com>
2020-12-05 22:32:04 -0900

    add: fltk template in proud-polish.cpp

--- hw8/least-finished/least-finished.cpp
+++ hw8/least-finished/least-finished.cpp
@@ -3,6 +3,10 @@
 // 2 December 2020
 // Least finished program example for CS 201.
 
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -13,6 +17,14 @@ using std::endl;
 using std::string;
 using std::map;
 
-int main() {
-    std::cout << "Hello, least finished program." << endl;
+int main(int argc, char **argv) {
+    Fl_Window *window = new Fl_Window(340,180);
+    Fl_Box *box = new Fl_Box(20,40,300,100,"cs201 hw8: \nTruncate & Quit");
+    box->box(FL_UP_BOX);
+    box->labelfont(FL_BOLD+FL_ITALIC);
+    box->labelsize(36);
+    box->labeltype(FL_SHADOW_LABEL);
+    window->end();
+    window->show(argc, argv);
+    return Fl::run();
 }

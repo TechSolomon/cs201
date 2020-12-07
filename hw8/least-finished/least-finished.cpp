51d319fff822dc40ec41edc89202fcaecf08fb55
Solomon Himelbloom <2001solomon@gmail.com>
2020-12-06 23:34:13 -0900

    add: truncate text and quit window button

--- hw8/least-finished/least-finished.cpp
+++ hw8/least-finished/least-finished.cpp
@@ -5,7 +5,9 @@
 
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Pixmap.H>
 
 #include <iostream>
 #include <stdio.h>
@@ -17,14 +19,33 @@ using std::endl;
 using std::string;
 using std::map;
 
+void TruncateText() {
+    cout << "FLTK: Truncate Text" << endl;
+    exit(1);
+}
+
+void QuitWindow() {
+    cout << "FLTK: Quit Window" << endl;
+    exit(2);
+}
+
 int main(int argc, char **argv) {
-    Fl_Window *window = new Fl_Window(340,180);
-    Fl_Box *box = new Fl_Box(20,40,300,100,"cs201 hw8: \nTruncate & Quit");
+
+    Fl_Window *window = new Fl_Window(340,180, "FLTK: cs201 hw8");
+
+    // Create the FLTK application window.
+    Fl_Box *box = new Fl_Box(20,40,300,100);
     box->box(FL_UP_BOX);
     box->labelfont(FL_BOLD+FL_ITALIC);
     box->labelsize(36);
     box->labeltype(FL_SHADOW_LABEL);
+
+    // Create two instances of regular buttons.
+    Fl_Button *but1 = new Fl_Button(100,60,140,25,"Truncate");
+    Fl_Button *but2 = new Fl_Button(100,90,140,25,"Quit");
+
     window->end();
     window->show(argc, argv);
     return Fl::run();
+
 }
